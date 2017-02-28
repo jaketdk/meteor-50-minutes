c4461d618a4b1c9ef4c5280dfc1e94299f94819a
jaketdk <jaketdk@hawaii.edu>
2017-02-27 20:43:39 -1000

    Late assignment

---
+++ app/tasklist.js
@@ -0,0 +1,60 @@
+/**
+ * Created by jake on 2/27/17.
+ */
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Meteor.subscribe('tasks');
+
+Template.tasks.helpers({
+  tasks: function(){
+    return Tasks.find({}, {sort: {createdAt: -1}});
+  }
+});
+
+Template.tasks.events({
+  "submit .add-task": function(event){
+    let name = event.target.name.value;
+
+    Meteor.call('addTask', name);
+
+    event.target.name.value = '';
+
+    return false;
+  },
+  "click .delete-task": function (event) {
+    if(confirm('Delete Task?')){
+      Meteor.call('deleteTask', this._id);
+    }
+
+    return false;
+  }
+
+})
+}
+
+if (Meteor.isServer) {
+
+  Meteor.publish('tasks', function(){
+    return Tasks.find({userId: this.userId});
+  });
+
+}
+
+Meteor.methods({
+  addTask: function (name) {
+    if(!Meteor.userId()){
+      throw new Metwor.Error('No Access!');
+    }
+
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userId: Meteor.userId()
+    });
+
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  }
+});
